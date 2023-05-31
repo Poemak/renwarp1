be39f6394f3e2d6f332a79459e30daef80a484a0
Poemak <125496896+Poemak@users.noreply.github.com>
2023-05-31 18:14:43 +0330

    domAIN2

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -220,7 +220,7 @@ generate_config() {
             {
                 "type":"field",
                 "domain":[
-                    "",
+                    ""
                 ],
                 "outboundTag":"WARP"
             }
